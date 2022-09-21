3a5e26dd6a924bc4d55813dc80a1c8003d24a316
Fabricegithubrit <nkurunziza2001gashati@gmail.com>
2022-09-21 04:27:21 -0700

    question 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,10 +3,14 @@
 /**
 * reset_to_98 - reset the value of given integer to 98
 * @n: paramenter
-* Return : Always 0
+* Return : Always 0 (Success)
 */
 
-void reset_to_98(int *n);
+int main(void)
+{
+	void reset_to_98(int *n)
 {
 *n = 98;
+}
+return (0);
 }
